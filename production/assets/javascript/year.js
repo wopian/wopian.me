87287e2039446d883ca17b8de05e90fed2d2d384
James Harris <wopiancitizen123@gmail.com>
2014-12-07 17:06:22 +0000

    HYR

--- production/assets/javascript/year.js
+++ production/assets/javascript/year.js
@@ -506,40 +506,68 @@
     }) : "undefined" != typeof module && module.exports ? module.exports = i : e.skrollr = i
 })(window, document);
 
-$(document).ready(function(){
+/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
+(function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
 
-    $("body").onepage_scroll({
-       sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
-       easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-       animationTime: 900, // AnimationTime let you define how long each section takes to animate
-       pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
-       updateURL: false // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-    });
+/**
+ * How to create a parallax scrolling website
+ * Author: Petr Tichy
+ * URL: www.ihatetomatoes.net
+ * Article URL: http://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website/
+ */
 
-});
-
-function init() {
-
-    // start up after 2sec no matter what
-    window.setTimeout(function(){
-        start();
-    }, 2000);
-}
-
-// fade in experience
-function start() {
-
-    $('body').removeClass("loading").addClass('loaded');
-
-}
-
-$(window).load(function() {
-
-    // fade in page
-    init();
-
-});
-
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$slideTall = $('.homeSlideTall');
+	$slideTall2 = $('.homeSlideTall2');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		var s = skrollr.init({
+		    render: function(data) {
+		    
+		        //Debugging - Log the current scroll position.
+		        //console.log(data.curTop);
+		    }
+		});
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    $slideTall.height(winH*2);
+	    $slideTall2.height(winH*3);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.homeSlide'));
+	    
+	}
+		
+} )( jQuery );
 /*Highcharts JS v4.0.4 (2014-09-02)
   (c) 2009-2014 Torstein Honsi
   License: www.highcharts.com/license*/
