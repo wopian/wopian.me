596770e2eaab6d86714095f91005962205f98311
James Harris <wopiancitizen123@gmail.com>
2014-12-10 15:20:45 +0000

    Revert "Update sass.js"
    This reverts commit ca0bd3f5761aff51cb2feb5b6f6c6d3d394386f3.

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -2,8 +2,7 @@ module.exports = {
   dist: {
     options: {
       // cssmin will minify later
-      style: 'expanded',
-      debugInfo: true
+      style: 'expanded'
     },
     files: {
       'assets/css/main.css': 'assets/sass/main.scss'
