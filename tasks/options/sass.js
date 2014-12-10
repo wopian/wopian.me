ca0bd3f5761aff51cb2feb5b6f6c6d3d394386f3
James Harris <wopiancitizen123@gmail.com>
2014-12-10 13:34:32 +0000

    Update sass.js

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -2,7 +2,8 @@ module.exports = {
   dist: {
     options: {
       // cssmin will minify later
-      style: 'expanded'
+      style: 'expanded',
+      debugInfo: true
     },
     files: {
       'assets/css/main.css': 'assets/sass/main.scss'
