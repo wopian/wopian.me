8d8bd6d81f7ede631b1808eeed62e1348430bfa2
James Harris <wopiancitizen123@gmail.com>
2014-12-10 13:25:47 +0000

    Update uglify.js

--- tasks/options/uglify.js
+++ tasks/options/uglify.js
@@ -1,8 +1,7 @@
 module.exports = {
   build: {
     files: {
-        'production/assets/javascript/app.min.js' : ['production/assets/javascript/app.js'],
-        'production/assets/javascript/year.min.js' : ['production/assets/javascript/year.js'],
+        'production/assets/javascript/app.min.js' : ['production/assets/javascript/app.js']
     }
   }
 }
