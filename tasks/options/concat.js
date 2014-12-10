b264391fc7d01eb83043ef4dcb1a4b047d40ad12
James Harris <wopiancitizen123@gmail.com>
2014-12-10 13:25:03 +0000

    Update concat.js

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -1,8 +1,7 @@
 module.exports = {
   dist: {
     files: {
-        'production/assets/javascript/app.js' : ['assets/javascript/skrollr.js', 'assets/javascript/main.js', 'assets/javascript/highcharts.js', 'assets/javascript/snow.js'],
-        'production/assets/javascript/imagesloaded.js' : ['assets/javascript/skrollr.js', 'assets/javascript/_year.js'],
+        'production/assets/javascript/app.js' : ['assets/javascript/skrollr.js', 'assets/javascript/main.js', 'assets/javascript/highcharts.js', 'assets/javascript/snow.js']
     }
   }
 }
