5a4741ebd0f22db2852db963d16a0705a08b328f
James Harris <wopiancitizen123@gmail.com>
2015-01-09 10:25:16 +0000

    Remove snow

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -1,7 +1,7 @@
 module.exports = {
   dist: {
     files: {
-        'production/assets/javascript/app.js' : ['assets/javascript/skrollr.js', 'assets/javascript/main.js', 'assets/javascript/highcharts.js', 'assets/javascript/snow.js']
+        'production/assets/javascript/app.js' : ['assets/javascript/skrollr.js', 'assets/javascript/main.js', 'assets/javascript/highcharts.js']
     }
   }
 }
