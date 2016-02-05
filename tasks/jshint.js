b76f09254e33816c51ad58be631939349f198a59
James Harris <wopiancitizen123@gmail.com>
2016-02-05 14:10:25 +0000

    Add jshint-stylish

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -1,4 +1,10 @@
 module.exports = {
+    options: {
+        reporter: require('jshint-stylish'),
+        globals: {
+            jQuery: true
+        },
+    },
     ignores: ['src/assets/js/bin/materialize.js'],
     beforeconcat: ['src/assets/js/*.js']
 }
