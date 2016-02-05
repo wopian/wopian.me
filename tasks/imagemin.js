746d62045b223710632c6addca71e59af2b91678
James Harris <wopiancitizen123@gmail.com>
2016-02-05 13:17:41 +0000

    Maybe fix imagemin?

--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -2,9 +2,9 @@ module.exports = {
     dynamic: {
         files: [{
             expand: true,
-            cwd: 'src/assets/images/',
+            cwd: 'src/assets/images',
             src: ['**/*.{png,jpg,gif}'],
-            dest: 'dist/assets/images/'
+            dest: 'dist/assets/images'
         }]
     }
 }
