37a3019dfd4c0c26b138b4d586fd70622389af31
James Harris <wopiancitizen123@gmail.com>
2016-02-26 01:38:43 +0000

    Add InstantClick JS

--- tasks/concat.js
+++ tasks/concat.js
@@ -6,6 +6,7 @@ module.exports = {
         files: {
             'dist/assets/js/app.js' : [
                 'src/assets/js/vendor/materialize.js',
+                'src/assets/js/vendor/instantclick.min.js',
                 'src/assets/js/init.js'
             ]
         }
