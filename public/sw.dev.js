6da7ff2f773e6e9bb1997b9ea616ac084e272316
James Harris <wopian@wopian.me>
2020-01-10 02:24:47 +0000

    feat: add service-worker for better GitHub Pages caching and offline access

---
+++ public/sw.dev.js
@@ -0,0 +1,55 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  workbox.routing.registerNavigationRoute('/index.html');
+
+  workbox.routing.registerRoute(
+    /\.(?:js|css|html)$/,
+    /\.index.html$/,
+    new workbox.strategies.StaleWhileRevalidate()
+  );
+
+   // Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  }),
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|svg|xml|ico|json)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'public',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    skipWaiting();
+  }
+});
+}
