e29240d37625a37e3198df6891d34cb97b012a85
James Harris <wopian@wopian.me>
2020-07-01 22:59:05 +0100

    build: use terser to compile sole js file

---
+++ client/sw.js
@@ -0,0 +1,17 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
+
+if (workbox) {
+  const handler = workbox.precaching.createHandlerBoundToURL('/index.html')
+  const navigationRoute = new workbox.routing.NavigationRoute(handler)
+
+  workbox.precaching.precacheAndRoute([
+    { url: '/favicon.svg', revision: '2020.Jul.01' },
+    { url: '/index.html', revision: '2020.Jul.01' },
+    { url: '/main.min.css', revision: '2020.Jul.01' },
+    { url: '/manifest.json', revision: '2020.Jul.01' },
+    { url: '/mask-icon.svg', revision: '2020.Jul.01' },
+  ])
+
+  workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate())
+  workbox.routing.registerRoute(navigationRoute)
+}
--- public/sw.js
+++
@@ -1,1 +0,0 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"),workbox&&workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate)
