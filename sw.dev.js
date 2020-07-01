2cdb475aec022424d1643aa6346d3d12b7ed940e
wopian <wopian@wopian.me>
2020-07-01 21:45:04 +0000

    chore: deploy c6e671a

--- sw.dev.js
+++ sw.dev.js
@@ -1,6 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
 if (workbox) {
+  workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate())
+
+  /*
   workbox.precaching.precacheAndRoute([
     { url: '/index.html', revision: '2' },
     { url: '/main.min.css', revision: '1' },
@@ -9,10 +12,11 @@ if (workbox) {
     { url: '/mask-icon.svg', revision: '1' },
     { url: '/apple-touch-icon.png', revision: '1' }
   ])
-  
+
   const handler = workbox.precaching.createHandlerBoundToURL('/index.html')
   const navigationRoute = new workbox.routing.NavigationRoute(handler)
-  workbox.routing.registerRoute(navigationRoute)
+
+  // workbox.routing.registerRoute(navigationRoute)
 
   workbox.routing.registerRoute(
     // Cache style resources, i.e CSS files
@@ -20,4 +24,5 @@ if (workbox) {
     // Use cache but update in the background
     new workbox.strategies.StaleWhileRevalidate({ cacheName: 'css-cache' })
   )
+  */
 }
