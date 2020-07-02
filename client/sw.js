0f0405610c4f4bbc0ebb36ee641a5f22907b5d3c
James Harris <wopian@wopian.me>
2020-07-02 01:19:15 +0100

    chore: replace node-sass with dart-sass

--- client/sw.js
+++ client/sw.js
@@ -1,9 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
 if (workbox) {
-  const handler = workbox.precaching.createHandlerBoundToURL('/index.html')
-  const navigationRoute = new workbox.routing.NavigationRoute(handler)
-
   workbox.precaching.precacheAndRoute([
     { url: '/favicon.svg', revision: '2020.Jul.01' },
     { url: '/index.html', revision: '2020.Jul.02' },
@@ -12,6 +9,15 @@ if (workbox) {
     { url: '/mask-icon.svg', revision: '2020.Jul.01' },
   ])
 
-  workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate())
+  const handler = workbox.precaching.createHandlerBoundToURL('/index.html')
+  const navigationRoute = new workbox.routing.NavigationRoute(handler)
+  const defaultStrategy = new workbox.strategies.StaleWhileRevalidate()
+
   workbox.routing.registerRoute(navigationRoute)
+  workbox.routing.setDefaultHandler(args => {
+    if (args.event.request.method === 'GET') {
+      return defaultStrategy.handle(args)
+    }
+    return fetch(args.event.request)
+  })
 }
