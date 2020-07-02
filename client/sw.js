dbffac0b034af743e90d9cda6d1213a7106ad631
James Harris <wopian@wopian.me>
2020-07-02 02:09:29 +0100

    chore: update sw

--- client/sw.js
+++ client/sw.js
@@ -2,11 +2,9 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 if (workbox) {
   workbox.precaching.precacheAndRoute([
-    { url: '/favicon.svg', revision: '2020.Jul.01' },
-    { url: '/index.html', revision: '2020.Jul.02' },
-    { url: '/main.min.css', revision: '2020.Jul.02' },
+    { url: '/index.html', revision: '2020.Jul.02-1' },
+    { url: '/main.min.css', revision: '2020.Jul.02-1' },
     { url: '/manifest.json', revision: '2020.Jul.01' },
-    { url: '/mask-icon.svg', revision: '2020.Jul.01' },
   ])
 
   const handler = workbox.precaching.createHandlerBoundToURL('/index.html')
